9d8ac5096122b97ccb100a1daf68178f2abb4039
Stephen Leavitt <stephen.leavitt@gmail.com>
2025-11-10 17:53:55 -0600

    Update Forge recipe

--- resources/forge-recipe.sh
+++ resources/forge-recipe.sh
@@ -1,16 +1,32 @@
-# Recipe Name: Install Docker and Docker-Compose
+# Recipe Name: Install Docker and Docker Compose
 # Run as user: root
 # Recipe:
-sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# Focal = Ubuntu 20.04
-# Run `lsb_release -cs` to discover yours if not 20.04
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-apt install -y docker-ce
-systemctl status docker
+# Uninstall any unofficial Docker packages
+apt autoremove docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc
+
+# Add Docker's official GPG key:
+apt update
+apt install ca-certificates curl
+install -m 0755 -d /usr/share/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /usr/share/keyrings/docker.asc
+chmod a+r /usr/share/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+cat << EOF > /etc/apt/sources.list.d/docker.sources
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Signed-By: /usr/share/keyrings/docker.asc
+Architectures: $(dpkg --print-architecture)
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+EOF
+
+# Install the Docker packages
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Ensure Docker is running
+systemctl start docker
+
+# Add the forge user to the docker group
 usermod -aG docker forge
-COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
-sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
-chmod +x /usr/local/bin/docker-compose
